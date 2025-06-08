ebb8c16cc87cff1c2964ce09c35ec92c3b6ffc18
Ben Hoff <hoff.benjamin.k@gmail.com>
2025-06-08 19:00:32 -0400

    missed file

---
+++ src/hws_v4l2_ctrl.h
@@ -0,0 +1,9 @@
+#ifndef HWS_V4L2_CTRL_H
+#define HWS_V4L2_CTRL_H
+
+#include <media/v4l2-ctrls.h>
+
+/* your control-ops */
+extern const struct v4l2_ctrl_ops hws_ctrl_ops;
+
+#endif /* HWS_V4L2_CTRL_H */
