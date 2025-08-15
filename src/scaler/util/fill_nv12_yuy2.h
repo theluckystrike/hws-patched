ea3f007d49b9cd0d27c0b0d3852293bf340a7efd
Ben Hoff <hoff.benjamin.k@gmail.com>
2025-08-14 20:21:47 -0400

    nothankyou

--- src/scaler/util/fill_nv12_yuy2.h
+++
@@ -1,9 +0,0 @@
-#ifndef FILL_NV12_YUY2_H
-#define FILL_NV12_YUY2_H
-#include <linux/types.h>
-
-void nv12_to_yuy2(u8 *src, u8 *dst,
-                  u16 width, u16 height,
-                  bool interlaced);
-
-#endif
