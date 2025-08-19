7ea050f285a17a8538ac93ea6350cce9950d8e3c
Ben Hoff <hoff.benjamin.k@gmail.com>
2025-08-19 04:17:00 -0400

    rm legacy

--- src/hws.h
+++ src/hws.h
@@ -9,6 +9,7 @@
 #include <linux/pci.h>
 #include <linux/spinlock.h>
 #include <linux/workqueue.h>
+#include <linux/sizes.h>
 
 #include <media/v4l2-ctrls.h>
 #include <media/v4l2-device.h>
@@ -16,33 +17,28 @@
 
 struct snd_pcm_substream;
 
-#define MAX_USER_IRQ 1
 #define XDMA_CHANNEL_NUM_MAX (1)
 #define MAX_NUM_ENGINES (XDMA_CHANNEL_NUM_MAX * 2)
 
 #define  PCIE_BARADDROFSIZE 4
 
+#define PCI_BUS_ACCESS_BASE       0x00000000U
+#define INT_EN_REG_BASE           (PCI_BUS_ACCESS_BASE + 0x0134U)
+#define PCIEBR_EN_REG_BASE        (PCI_BUS_ACCESS_BASE + 0x0148U)
+#define PCIE_INT_DEC_REG_BASE     (PCI_BUS_ACCESS_BASE + 0x0138U)
 
-#define PCI_BUS_ACCESS_BASE 0x0
+
 #define PCIEBAR_AXI_BASE 0x20000000
 
 #define CTL_REG_ACC_BASE 0x0
 #define PCI_ADDR_TABLE_BASE CTL_REG_ACC_BASE
 
-#define INT_EN_REG_BASE  PCI_BUS_ACCESS_BASE+0x134
-#define PCIEBR_EN_REG_BASE  PCI_BUS_ACCESS_BASE+0x148
-#define PCIE_INT_DEC_REG_BASE PCI_BUS_ACCESS_BASE+0x138
+#define CVBS_IN_BASE              0x00004000U
+#define CVBS_IN_BUF_BASE          (CVBS_IN_BASE + (16U * PCIE_BARADDROFSIZE))
+#define CVBS_IN_BUF_BASE2         (CVBS_IN_BASE + (50U * PCIE_BARADDROFSIZE))
 
-
-#define CVBS_IN_BASE 0x04000
-#define CBVS_IN_BUF_BASE CVBS_IN_BASE+ 16*PCIE_BARADDROFSIZE
-#define CBVS_IN_BUF_BASE2 CVBS_IN_BASE+ 50*PCIE_BARADDROFSIZE
 
 #define MAX_L_VIDEO_SIZE			0x200000	//2M
-
-#define MAX_VIDEO_PKSIZE            2
-#define HALF_VIDEO_PKSIZE           1
-
 
 
 #define PCI_E_BAR_PAGE_SIZE 0x20000000
@@ -51,38 +47,16 @@ struct snd_pcm_substream;
 
 #define MAX_DMA_AUDIO_PK_SIZE      128*16*2
 
-#define MEM_LOCK					1
-#define MEM_UNLOCK					0
-
 
 #define PAGESIZE					4096
 
-
-#define MAX_AUDIO_CAP_SIZE			10*1024			// 16KHz*2Byte*8channel/4(0.25sec)/4096(pagesize)
-#define MAX_AUDIO_QURE_INDEX        8
-#define MAX_AUDIO_LINE   4
-#define MAX_AUDIO_SUBCHANNEL		24
-#define MAX_DMA_PACK_SIZE 160
-
-
-#define MAX_AUDIO_EVENT_CNT 24
-#define MAX_AUDIO_BUF_CNT 50
-#define MAX_AUDIO_QUEUE_CNT 20
-#define MAX_AUDIO_BUF_SIZE 1920
-
 #define MAX_VID_CHANNELS            4
-#define MAX_VIDEO_QUEUE				4
-#define MAX_AUDIO_QUEUE				8
 
-#define CUR_AUDIO_PACKET_LENGTH		4096+34						// 250ms,  2*16,000=32,000 -> 1s
-#define AUDIO_ONE_QUEUE_SIZE		8*CUR_AUDIO_PACKET_LENGTH
-
-#include <linux/sizes.h>
 #define MAX_MM_VIDEO_SIZE            SZ_4M
 
 #define MAX_VIDEO_HW_W 1920
 #define MAX_VIDEO_HW_H 1080
-#define MAX_VIDEO_SCALER_SIZE 1920*1080*2
+#define MAX_VIDEO_SCALER_SIZE     (1920U * 1080U * 2U)
 
 struct hwsmem_param 
 {
@@ -110,28 +84,6 @@ struct hws_video_fmt {
 	u32 half_size;
 	u32 down_size;
 };
-
-
-#define HWS_IOC_MAGIC      'd'
-
-enum hws_ioc_cmd {
-	HWS_IOC_MAP_BUFFER,
-	HWS_IOC_START_DMA,
-	HWS_IOC_STOP_DMA,
-	HWS_IOC_GET_CAPTURE,
-	HWS_IOC_SET_FORMAT,
-};
-
-#define HWS_IOC_MAP_BUFFER     _IOWR(HWS_IOC_MAGIC, HWS_IOC_MAP_BUFFER,  struct hwsmem_param)
-#define HWS_IOC_START_DMA      _IOWR(HWS_IOC_MAGIC, HWS_IOC_START_DMA,   struct hwsmem_param)
-#define HWS_IOC_STOP_DMA       _IOWR(HWS_IOC_MAGIC, HWS_IOC_STOP_DMA,    struct hwsmem_param)
-#define HWS_IOC_GET_CAPTURE    _IOWR(HWS_IOC_MAGIC, HWS_IOC_GET_CAPTURE, struct hwsmem_param)
-#define HWS_IOC_SET_FORMAT     _IOWR(HWS_IOC_MAGIC, HWS_IOC_SET_FORMAT,  struct hwsmem_param)
-
-//--------------------
-
-#define HWS_PCIE_WRITE(__addr, __offst, __data)	writel((__data), (dev->mmio + (__addr + __offst)))
-#define HWS_PCIE_READ(__addr, __offst)		readl((dev->mmio + (__addr + __offst)))
 
 
 #define	UNSET	(-1U)
